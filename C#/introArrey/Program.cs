2b755bc6917cfdc4d62a7cfeda75ffe3fb975b6a
Aleksey <teleccabaka@example.com>
2022-08-10 21:24:16 +0500

    списал код с лекции

---
+++ C#/introArrey/Program.cs
@@ -0,0 +1,45 @@
+﻿void FillArray (int[]collection)
+{
+int length = collection.Length;
+int index =0;
+while (index<length)
+{
+    collection[index]=new Random() .Next(1,10);
+    index++;
+}
+}
+
+void PrintArray(int []col)//воид название метода(размерность[]название массива)
+{
+    int count= col.Length;//количество элементов
+    int position =0;//позиция в массиве
+    while (position<count)//условие
+    {
+        Console.WriteLine(col[position]);
+        position++;//индекс+1
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position =-1;    
+    while(index<count)
+    {
+        if(collection[index]==find)
+        {
+         position=index;
+         break;
+        }
+        index++;
+    }
+    return position;
+}
+int[] array =new int[10];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+int pos= IndexOf(array,2);
+Console.WriteLine(pos);
